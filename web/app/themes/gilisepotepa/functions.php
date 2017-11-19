ae863c97b1a5fa029f1aed612088efbcd72abc9d
Primoz Cigler <primoz@proteusnet.com>
2017-11-19 18:02:37 +0100

    Add Readable and child theme

---
+++ web/app/themes/gilisepotepa/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions of the child theme for Readable WP
+ */
+
+add_action( 'wp_enqueue_scripts', 'add_additional_css', 20 );
+/**
+ * Add the style.css (from this folder) after the main.css file
+ *
+ * @link http://codex.wordpress.org/Function_Reference/wp_enqueue_style Codex for wp_enqueue_style()
+ */
+function add_additional_css() {
+	wp_enqueue_style( 'child-css', get_stylesheet_uri() , array( 'main-css' ) );
+}
